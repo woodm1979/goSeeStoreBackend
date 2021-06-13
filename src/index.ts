468539fb2f218bd0456eec5222d584397c4f08a9
woodm1979 <woodm1979@gmail.com>
2021-06-12 20:51:46 -0600

    add some todo routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express from 'express';
-import { json } from 'body-parser';
+import express from 'express'
+import { json } from 'body-parser'
+import { todoRouter } from './routes/todo'
 
 const app = express()
 app.use(json())
+app.use(todoRouter)
 
 app.listen(3000, () => {
   console.log('server is listening on port 3000')
