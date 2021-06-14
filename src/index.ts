2350a6770bd359bf6478d675d28acf74753459af
woodm1979 <woodm1979@gmail.com>
2021-06-14 01:03:00 -0600

    Setup Apollo Server and setup some initial types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
+import { ApolloServer } from 'apollo-server'
+import { json } from 'body-parser'
 import express from 'express'
 import mongoose from 'mongoose'
-import { json } from 'body-parser'
+
 import { todoRouter } from './routes/todo'
+import { resolvers, typeDefs } from './schema'
 
-const app = express()
-app.use(json())
-app.use(todoRouter)
-
-// mongoose.connect('mongodb://localhost:27017/')
 mongoose.connect(
   'mongodb://127.0.0.1:27017/goSeeStore?compressors=zlib&gssapiServiceName=mongodb',
   {
@@ -20,6 +18,22 @@ mongoose.connect(
   }
 )
 
+// Express REST server
+const app = express()
+app.use(json())
+app.use(todoRouter)
 app.listen(3000, () => {
-  console.log('server is listening on port 3000')
+  console.log('REST server is listening on port 3000')
+})
+
+// GraphQL server
+const graphQLServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+graphQLServer.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+  `)
 })
---
+++ src/schema.ts
@@ -0,0 +1,68 @@
+import { gql } from 'apollo-server'
+import { GraphQLScalarType, Kind } from 'graphql'
+
+export const typeDefs = gql`
+  scalar Date
+
+  type Query {
+    hello: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+  }
+
+  type Store {
+    id: ID!
+    name: String!
+  }
+
+  type Ingredient {
+    id: ID!
+    name: String!
+  }
+
+  type ShoppingList {
+    id: ID!
+    name: String
+  }
+
+  type StoreIngredient {
+    id: ID!
+    store: Store!
+    ingredient: Ingredient!
+    aisle: String
+  }
+
+  type ShoppingListIngredient {
+    id: ID!
+    ingredient: Ingredient!
+    dateAdded: Date!
+    dateCheckedOff: Date
+  }
+`
+
+const dateScalar = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  serialize(value) {
+    return value.getTime() // Convert outgoing Date to integer for JSON
+  },
+  parseValue(value) {
+    return new Date(value) // Convert incoming integer to Date
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)) // Convert hard-coded AST string to integer and then to Date
+    }
+    return null // Invalid hard-coded value (not an integer)
+  },
+})
+
+export const resolvers = {
+  Date: dateScalar,
+  Query: {
+    hello: () => 'Hello World!',
+  },
+}
